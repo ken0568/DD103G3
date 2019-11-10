6515a6be2aeb6a6313904551b3556a5e0bff7797
b10332040 <b10332040@gmail.com>
2019-11-10 16:31:14 +0800

    活動專區-嵌入地圖、修改部分funciton

--- js/activities-main.js
+++ js/activities-main.js
@@ -1,5 +1,5 @@
-function init(){
-  function id(id){
+function init() {
+  function id(id) {
     return document.getElementById(id);
   }
 
@@ -12,6 +12,16 @@ function init(){
     opacity: 1,
   }, .5);
 
+  var ScreenWidth = screen.width;
+
+  //--------resize
+  $(window).resize(function () {
+    var actionRange = document.querySelectorAll('.activityContent-actInfo .right')[0].offsetHeight;
+    var mapHeight = document.querySelectorAll('.activityContent-actInfo .map')[0].offsetHeight;
+    var signBoxHeight = document.querySelectorAll('.activityContent-actInfo .signBox')[0].offsetHeight;
+    var ScreenWidth = screen.width;
+  });
+
   //---------依頁面標題載入function
   switch (document.title) {
     case "活動專區--鏟屎官":
@@ -19,16 +29,19 @@ function init(){
       changeActsSort();
       actsBanner();
       break;
-  
+
     case "活動內容--鏟屎官":
       pawsMove2();
       actSignUp();
+      signBoxMove();
+      showMap();
+      breadCrumbs();
       break;
   }
 
-  //activities.html 活動專區
+  //========= activities.html 活動專區 ==========//
   //----------------------------------------slider
-  function actsBanner(){
+  function actsBanner() {
     let bannerDivWidth = 100;
     let bannerLis = document.querySelectorAll('.pics li');
     let bannerCounts = document.querySelectorAll('.pics li').length;
@@ -80,13 +93,13 @@ function init(){
           left: `${(-sliderCounts) * 100}%`,
         }, 1000, 'linear');
       }
-      
+
       console.log(`自動輪播sliderCounts: ${sliderCounts}`);
       sliderCounts++;
     };//banner自動輪播End
 
     //click dot
-    $('.activities-header .dotsWrap li').click(function(){
+    $('.activities-header .dotsWrap li').click(function () {
       let dotIndex = $(this).index();
       sliderCounts = dotIndex;
       //重啟自動輪播
@@ -104,18 +117,18 @@ function init(){
   }//sliderEnd
 
   //------------------------------------------活動類別按鈕切換
-  function changeActsSort(){
+  function changeActsSort() {
     let pageActsBtn = document.querySelectorAll('.acts-btns .btn');
 
     //切換大標文字
-    for(var i=0; i<pageActsBtn.length; i++){
-      pageActsBtn[i].addEventListener('click',function(e){
+    for (var i = 0; i < pageActsBtn.length; i++) {
+      pageActsBtn[i].addEventListener('click', function (e) {
         //let btnTitle = document.querySelector(`${this.nodeName} p`).innerText;
         var btnTitle = e.target.innerText;
         //let btnTitle = document.querySelector(`${this.tagName} p`).innerText;
         //let btnTitle = document.querySelector(`${e.currentTarget.tagName} p`).innerText;
         //console.log(btnTitle);
-        
+
         //清除其他按鈕顏色
         cleanBtnBgc();
 
@@ -127,31 +140,31 @@ function init(){
       });
     }
     //清除其他按鈕顏色
-    function cleanBtnBgc(){
-      for (var i = 0; i < pageActsBtn.length; i++){
+    function cleanBtnBgc() {
+      for (var i = 0; i < pageActsBtn.length; i++) {
         pageActsBtn[i].style.backgroundColor = "#028090";
       }
     };
   }; //活動類別按鈕切換End
- 
+
   //----------------------------------------腳步移動動畫
   function pawsMove() {
     var actionRange = id("acts-paws").offsetHeight;
-    if(screen.width < 768){
+    if (ScreenWidth < 768) {
       var scene = new ScrollMagic.Scene({
         triggerElement: '#paws-trigger',
         triggerHook: .3,
         duration: actionRange * .3,
         offset: actionRange * .2
       }).setTween(pawAnime).addTo(smController);
-    } else if (screen.width < 1200) {
+    } else if (ScreenWidth < 1200) {
       var scene = new ScrollMagic.Scene({
         triggerElement: '#paws-trigger',
         triggerHook: .52,
         duration: actionRange * .32,
         offset: actionRange * .1
       }).setTween(pawAnime).addTo(smController);
-    } else{
+    } else {
       var scene = new ScrollMagic.Scene({
         triggerElement: '#paws-trigger',
         triggerHook: .58,
@@ -159,28 +172,28 @@ function init(){
         offset: actionRange * .1
       }).setTween(pawAnime).addTo(smController);
     }
-  } //腳步移動動畫End
-  
+  }; //腳步移動動畫End
 
-  //activityContent.html 活動內頁
+
+  //========= activityContent.html 活動內頁 =========//
   //----------------------------------------腳步移動動畫
-  function pawsMove2(){
+  function pawsMove2() {
     var actionRange = id("acts-paws2").offsetHeight;
-    if (screen.width < 768) {
+    if (ScreenWidth < 768) {
       var scene = new ScrollMagic.Scene({
         triggerElement: '#paws-trigger',
         triggerHook: .3,
         duration: actionRange * .4,
         offset: actionRange * .15
       }).setTween(pawAnime).addTo(smController);
-    } else if (screen.width < 1025) {
+    } else if (ScreenWidth < 1025) {
       var scene = new ScrollMagic.Scene({
         triggerElement: '#paws-trigger',
         triggerHook: .45,
         duration: actionRange * .2,
         offset: actionRange * .12
       }).setTween(pawAnime).addTo(smController);
-    } else if (screen.width < 1200) {
+    } else if (ScreenWidth < 1200) {
       var scene = new ScrollMagic.Scene({
         triggerElement: '#paws-trigger',
         triggerHook: .45,
@@ -195,36 +208,90 @@ function init(){
         offset: actionRange * .1
       }).setTween(pawAnime).addTo(smController);
     }
-  }//腳步移動動畫結束
+  };//腳步移動動畫結束
+
+  //----------------------------------------右側signBox移動
+  function signBoxMove() {
+    var actionRange = document.querySelectorAll('.activityContent-actInfo .right')[0].offsetHeight;
+    var mapHeight = document.querySelectorAll('.activityContent-actInfo .map')[0].offsetHeight;
+    var signBoxHeight = document.querySelectorAll('.activityContent-actInfo .signBox')[0].offsetHeight;
+    //console.log(actionRange);
+
+    if (ScreenWidth > 767) {
+      let scollDownAnime = TweenMax.staggerFromTo('.signBox', 2, {
+        y: 0
+      }, {
+        y: actionRange - mapHeight + signBoxHeight,
+      }, 0.5);
+
+      var scene = new ScrollMagic.Scene({
+        triggerElement: '#signBox-trigger',
+        triggerHook: .3,
+        duration: actionRange,
+        offset: actionRange * .05
+      }).setTween(scollDownAnime).addTo(smController);
+    }
+  }; //右側signBox移動End
 
   //----------------------------------------活動報名
-  function actSignUp(){
+  function actSignUp() {
     var clickCounts = 0;
-    id("actSignBtn").addEventListener('click',function(e){
+    id("actSignBtn").addEventListener('click', function (e) {
       let actSignBtnText = e.target;
-      console.log(actSignBtnText);
-
+      id('actSignBtn').disabled = true;
       let actSigntimer;
-      
 
       //顯示報名成功
       id('actsSignSuccessLightBox').style.display = '';
-      actSigntimer = setTimeout(closeSuccessBox, 2500);
+      actSigntimer = setTimeout(closeSuccessBox, 2000);
       clickCounts++;
-      //關掉lightBox
+      //關掉lightBox，click顯示成功lightBOX外面or自動消失
+      id('actsSignSuccessLightBox').addEventListener('click', function () {
+        id('actsSignSuccessLightBox').style.display = 'none';
+        id('actSignBtn').style.backgroundColor = '#d8d8d8';
+        id('actSignBtn').style.cursor = 'default';
+        id('thisActNowNum').style.display = 'none';
+        actSignBtnText.innerText = '已報名';
+      });
       function closeSuccessBox() {
         id('actsSignSuccessLightBox').style.display = 'none';
         id('actSignBtn').style.backgroundColor = '#d8d8d8';
         id('actSignBtn').style.cursor = 'default';
         id('thisActNowNum').style.display = 'none';
         actSignBtnText.innerText = '已報名';
-        id('actSignBtn').disabled = true;
       };
-    }); 
-    
-  } //活動報名End
+    });
 
+  }; //活動報名End
+
+  //----------------------------------------活動地圖
+  function showMap() {
+    let area = id('actMap');
+    let actLatLng = new google.maps.LatLng(25.000618, 121.463370);
+    let options = {
+      zoom: 14,
+      center: actLatLng,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+    };
+    let actMap = new google.maps.Map(area, options);
+    let marker = new google.maps.Marker({
+      position: actLatLng,
+      map: actMap,
+      icon: '../img/activities_mapLocation.png',
+    });
+  } //活動地圖End
+
+  //----------------------------------------麵包屑
+  function breadCrumbs() {
+    let breadCrumbsLast = document.querySelectorAll('.breadCrumbs li:last-child')[0];
+    let actBigTitle = document.querySelectorAll('.actBigName')[0];
+    $('.breadCrumbs').append('<li><a href="activities.html">活動專區</a></li>');
+    //第二個需要判斷在前一頁是否有click頁籤
+    $('.breadCrumbs').append('<li><a href="activities.html">分享講座</a></li>');
+    //麵包屑最後一項跟活動標題一樣
+    $('.breadCrumbs').append(`<li>${actBigTitle.innerText}</li>`);
+  } //麵包屑End
 
 };
 
-window.addEventListener("load",init,false);
+window.addEventListener("load", init, false);
